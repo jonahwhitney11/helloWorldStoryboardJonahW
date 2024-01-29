a4faca9bfca08b2bc836173b42ea817928701140
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-01-29 12:30:40 -0600

    Added label and button connections. Added text field that takes user input and updates the label upon pressing the button.

--- helloWorldStoryboard/ViewController.swift
+++ helloWorldStoryboard/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelResult: UILabel!
+    
+    @IBOutlet weak var textMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
 
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textMessage.text
+    }
 }
 
